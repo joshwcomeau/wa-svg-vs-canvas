f20cc3dded76583e1b3b2119a2ccbd09c56d15c4
Joshua Comeau <joshwcomeau@gmail.com>
2024-12-29 16:14:01 -0500

    Index route

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
-import ReactCanvas from './routes/ReactCanvas';
-import ReactSvg from './routes/ReactSvg';
+import App from './App';
+import ReactCanvas from './routes/ReactCanvas/';
+import ReactSvg from './routes/ReactSvg/';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/react-canvas" element={<ReactCanvas />} />
         <Route path="/react-svg" element={<ReactSvg />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
